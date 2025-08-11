c6f2a539f930e6534201c99151ed56d50fc00842
Troy Makaro <troy@makaro.com>
2025-08-11 14:46:44 -0700

    allow outgoing email to not require a user or a password

--- OpenVmsTextEditor.Web/OpenVmsTextEditor.Infrastructure/SmtpEmailSender.cs
+++ OpenVmsTextEditor.Web/OpenVmsTextEditor.Infrastructure/SmtpEmailSender.cs
@@ -12,9 +12,15 @@ public sealed class SmtpEmailSender : IEmailSender
     {
         using var client = new System.Net.Mail.SmtpClient(_opt.Host, _opt.Port)
         {
-            EnableSsl = _opt.EnableSsl,
-            Credentials = new System.Net.NetworkCredential(_opt.User, _opt.Password)
+            EnableSsl = _opt.EnableSsl
         };
+
+        // Only set credentials if password is provided
+        if (!string.IsNullOrWhiteSpace(_opt.Password))
+        {
+            client.Credentials = new System.Net.NetworkCredential(_opt.User, _opt.Password);
+        }
+
         using var msg = new System.Net.Mail.MailMessage(_opt.From, email, subject, htmlMessage)
             { IsBodyHtml = true };
         await client.SendMailAsync(msg);
